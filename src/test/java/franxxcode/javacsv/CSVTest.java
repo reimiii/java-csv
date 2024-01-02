f3dd4416560cefb31652ed37124dd226ad1cebb6
Hilmi AM <imiia75775@proton.me>
2024-01-02 16:28:27 +0700

    Format CSV, done

--- src/test/java/franxxcode/javacsv/CSVTest.java
+++ src/test/java/franxxcode/javacsv/CSVTest.java
@@ -85,4 +85,23 @@ public class CSVTest {
 
         assertNotNull(writer);
     }
+    @Test
+    void createCSVWithTab() throws IOException {
+
+        StringWriter writer = new StringWriter();
+
+        CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.TDF.builder()
+                .setHeader("Name", "Status", "Value")
+                .build());
+
+        csvPrinter.printRecord("Mee", true, 100);
+        csvPrinter.printRecord("Qee", true, 100);
+        csvPrinter.flush();
+
+        System.out.println(
+                writer.getBuffer().toString()
+        );
+
+        assertNotNull(writer);
+    }
 }
