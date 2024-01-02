0291d240083535f24f8be5ab4702b760467f2e33
Hilmi AM <imiia75775@proton.me>
2024-01-02 15:25:59 +0700

    Initial commit

---
+++ src/main/java/franxxcode/javacsv/JavaCsvApplication.java
@@ -0,0 +1,13 @@
+package franxxcode.javacsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franxxcode/javacsv/JavaCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package franxxcode.javacsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
