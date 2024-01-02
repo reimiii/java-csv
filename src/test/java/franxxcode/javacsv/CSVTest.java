40ac5c5f86e0fca8d3892125001378b8ecc861f3
Hilmi AM <imiia75775@proton.me>
2024-01-02 16:15:47 +0700

    read csv

--- src/test/java/franxxcode/javacsv/CSVTest.java
+++ src/test/java/franxxcode/javacsv/CSVTest.java
@@ -1,11 +1,15 @@
 package franxxcode.javacsv;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,5 +31,20 @@ public class CSVTest {
         );
 
         assertNotNull(writer);
+    }
+
+    @Test
+    void readCSV() throws IOException {
+
+        CSVParser records = new CSVParser(
+                Files.newBufferedReader(Path.of("sample.csv")),
+                CSVFormat.DEFAULT
+        );
+
+        for (CSVRecord record : records) {
+            System.out.println("Name: " + record.get(0));
+            System.out.println("Success: " + record.get(1));
+            System.out.println("Value: " + record.get(2));
+        }
     }
 }
