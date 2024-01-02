39c81e7df285750d183e8e1884faa812ba854d23
Hilmi AM <imiia75775@proton.me>
2024-01-02 16:25:24 +0700

    csv Header

--- src/test/java/franxxcode/javacsv/CSVTest.java
+++ src/test/java/franxxcode/javacsv/CSVTest.java
@@ -47,4 +47,42 @@ public class CSVTest {
             System.out.println("Value: " + record.get(2));
         }
     }
+
+    @Test
+    void readCSVHeaderFirstLine() throws IOException {
+
+        CSVParser records = new CSVParser(
+                Files.newBufferedReader(Path.of("sample.csv")),
+                CSVFormat.DEFAULT.builder()
+                        .setHeader()
+                        .build()
+        );
+
+        for (CSVRecord record : records) {
+            System.out.println("name: " + record.get("username"));
+            System.out.println("status: " + record.get("status"));
+            System.out.println("value: " + record.get("value"));
+        }
+    }
+
+
+    @Test
+    void createWithHeader() throws IOException {
+
+        StringWriter writer = new StringWriter();
+
+        CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.builder()
+                .setHeader("Name", "Status", "Value")
+                .build());
+
+        csvPrinter.printRecord("Mee", true, 100);
+        csvPrinter.printRecord("Qee", true, 100);
+        csvPrinter.flush();
+
+        System.out.println(
+                writer.getBuffer().toString()
+        );
+
+        assertNotNull(writer);
+    }
 }
